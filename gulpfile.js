8f32ad09e25cd9ffdad652d635f6189be472ea52
Jeff George <webdevjeffus@gmail.com>
2017-02-20 01:45:47 -0500

    Install browser-sync and integrate into gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,17 @@ var autoprefixer = require("autoprefixer");
 var cssVars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
+var browserSync = require("browser-sync").create();
 
 gulp.task("default", function() {
   console.log("You ran default gulp task.");
 });
 
 gulp.task("html", function() {
-
+  browserSync.reload();
 });
 
-gulp.task("styles", function() {
+gulp.task("buildCSS", function() {
   return gulp.src( "./app/assets/css/styles.css" )
     .pipe( postcss([cssImport, cssVars, nested, autoprefixer]))
     .pipe(gulp.dest( "./app/temp/css" ));
@@ -22,11 +23,23 @@ gulp.task("styles", function() {
 
 gulp.task("watch", function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch("./app/index.html", function() {
     gulp.start("html");
   });
 
   watch("./app/assets/css/**/*.css", function() {
-    gulp.start("styles")
-  })
-})
+    gulp.start("updateCSS");
+  });
+});
+
+gulp.task("updateCSS", ["buildCSS"],function() {
+  return gulp.src("./app/temp/css/styles.css")
+    .pipe(browserSync.stream());
+});
