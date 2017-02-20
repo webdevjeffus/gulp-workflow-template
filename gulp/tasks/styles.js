d60aefd1b49d140d939f12474eaf1f3237c33df2
Jeff George <webdevjeffus@gmail.com>
2017-02-20 02:55:37 -0500

    Install and deploy postcss-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,20 +1,16 @@
 var gulp =          require("gulp");
 var postcss =       require("gulp-postcss");
-var autoprefixer =  require("autoprefixer");
+var cssImport =     require("postcss-import");
+var mixins =        require("postcss-mixins");
 var cssVars =       require("postcss-simple-vars");
 var nested =        require("postcss-nested");
-var cssImport =     require("postcss-import");
+var autoprefixer =  require("autoprefixer");
 var browserSync =   require("browser-sync");
 
 
-gulp.task("updateCSS", ["buildCSS"], function() {
-  return gulp.src("./app/temp/css/styles.css")
-    .pipe(browserSync.stream());
-});
-
 gulp.task("buildCSS", function() {
   return gulp.src( "./app/assets/css/styles.css" )
-    .pipe( postcss([cssImport, cssVars, nested, autoprefixer]))
+    .pipe( postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
     .on("error", function(error) {
       console.log(error.toString());
       this.emit("end");
@@ -22,3 +18,8 @@ gulp.task("buildCSS", function() {
     .pipe(gulp.dest( "./app/temp/css" ));
 });
 
+
+gulp.task("updateCSS", ["buildCSS"], function() {
+  return gulp.src("./app/temp/css/styles.css")
+    .pipe(browserSync.stream());
+});
