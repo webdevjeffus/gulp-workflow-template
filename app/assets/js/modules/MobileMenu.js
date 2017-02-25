db9a684754bf254d70f158199bcc1e3e4c6ac88f
Jeff George <webdevjeffus@gmail.com>
2017-02-24 20:55:48 -0500

    Code and style responsive main nav menu

---
+++ app/assets/js/modules/MobileMenu.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+
+class MobileMenu {
+  constructor() {
+    this.navIcon = $("#site-header__nav-icon");
+    this.navList = $("#site-header__nav-list");
+    this.events();
+  }
+
+  events() {
+    this.navIcon.click(this.toggleNav.bind(this));
+  }
+
+  toggleNav() {
+    console.log("Nav icon clicked");
+    this.navList.toggleClass("site-header__nav-list--revealed");
+    this.navIcon.toggleClass("fa-bars");
+    this.navIcon.toggleClass("fa-close");
+  }
+}
+
+export default MobileMenu;
